9548502cdbcb548298ae60564ea403bad97957cb
Lalit Kumar <45649399+hellolalit@users.noreply.github.com>
2025-08-05 14:56:49 +0530

    updated sql folder

--- javascript/project/dynamic-todo-list/app.js
+++ javascript/project/dynamic-todo-list/app.js
@@ -7,9 +7,9 @@ function addtask() // function created.
     if (task.trim() !== "") { // checking input is not a empty string.
         newtask.push(task);  // ✅ Data added to array.
         console.log(newtask); // printing array for checking.
-        const li = document.createElement("li");
-        li.innerText = task;
-        list_container.appendChild(li);
+        const li = document.createElement("li"); // created new <li> tag.
+        li.innerText = task; // adding task(user input) in <li>.
+        list_container.appendChild(li); // adding li into selected ul.
     } else {
         alert("Please Enter a Valid task!");
     }
